bce6003fc7642193b540deff7c2e92de9d4b1860
Y0ni0 <eshcolsam@gmail.com>
2021-12-22 12:00:56 +0300

    controllers added

---
+++ app.js
@@ -0,0 +1,42 @@
+// BUDGET CONTROLER
+var budgetController = (function(){
+   
+    //some code
+
+})(); 
+
+
+// UI CONTROLLER
+var UIController = (function(){
+
+    //Code
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+    var ctrlAddItem = function(){
+
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add item to the UI
+
+        // 4. Calculate the budget 
+
+        // 5. Display the budget on the UI
+        console.log('enter key was pressed');
+    }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        
+        if( event.keyCode === 13 || event.which === 13 ){
+            ctrlAddItem();
+        }
+    })
+
+})(budgetController, UIController);
