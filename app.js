6bd9af981f4b7d6fa4668ca2057588f2d521c2c3
Y0ni0 <eshcolsam@gmail.com>
2021-12-23 11:01:43 +0300

    clear fields bug fixed

--- app.js
+++ app.js
@@ -99,6 +99,17 @@ var UIController = (function(){
             document.querySelector(element).insertAdjacentHTML('beforeend', newhtml);
 
         },
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+            fieldsArr[0].focuse();
+        },
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -130,9 +141,11 @@ var controller = (function(budgetCtrl, UICtrl){
         newItem = budgetCtrl.addItem(input.type, input.description, input.value); 
         // 3. Add item to the UI
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget 
+        // 4. clear the fields
+        UICtrl.clearFields();
+        // 5. Calculate the budget 
 
-        // 5. Display the budget on the UI 
+        // 6. Display the budget on the UI 
         
     };
 
